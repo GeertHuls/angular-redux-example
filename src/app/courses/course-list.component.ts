22ce0437d33d771c9ef8042f52225793d11d4e82
Geert Huls <geert.huls82@gmail.com>
2017-03-29 16:11:12 +0200

    Use NgRedux to connect redux with component using DI

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CourseService } from './course.service';
 import { Course } from './course';
 import { FilterTextComponent } from '../blocks/filter-text';
-import { store, filterCourses } from '../store';
+import { store, filterCourses, IAppState } from '../store';
+import { NgRedux, select } from 'ng2-redux';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-course-list',
@@ -10,9 +12,10 @@ import { store, filterCourses } from '../store';
   styleUrls: ['./course-list.component.css']
 })
 export class CourseListComponent implements OnInit {
-  filteredCourses = [];
+  // use the select annotation function to subscribe to the store:
+  @select('filteredCourses') filteredCourses$: Observable<Course>;
 
-  constructor(private _courseService: CourseService) {
+  constructor(private ngRedux: NgRedux<IAppState>) {
   }
 
   filterChanged(searchText: string) {
@@ -20,16 +23,7 @@ export class CourseListComponent implements OnInit {
     store.dispatch(filterCourses(searchText));
   }
 
-  updateFromState() {
-    const allState = store.getState();
-    this.filteredCourses = allState.filteredCourses;
-  }
-
   ngOnInit() {
-    this.updateFromState();
-    store.subscribe(() => {
-      this.updateFromState();
-    });
     componentHandler.upgradeDom();
   }
 }
